f92ef2816a9edd02a963faf65e8f4dbc2eb3d88b
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-04 12:20:30 +0900

    uodate-deploy-config

---
+++ config/deploy.rb
@@ -0,0 +1,39 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.12.0'
+
+set :linked_files, %w{ config/secrets.yml }
+# Capistranoのログの表示に利用する
+set :application, 'furima-sumple-70a'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:mother-art/furima-sumple-70a.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/ChatSpace.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    # unicornを完全にキルしてから再起動する
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+  after :finishing, 'deploy:cleanup'
+end
