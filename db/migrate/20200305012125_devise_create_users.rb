1355d8dc117fa21de014899f6b682deed4511118
minorutaso <minorutaso@gmail.com>
2020-03-05 20:22:58 +0900

    姓名追加

--- db/migrate/20200305012125_devise_create_users.rb
+++ db/migrate/20200305012125_devise_create_users.rb
@@ -2,8 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     def change
       create_table :users do |t|
         t.string :nickname,           null:false  
-        t.string :name,               null:false  
-        t.string :name_kana,          null:false  
+        t.string :first_name,         null:false
+        t.string :last_name           null:false
+        t.string :first_name_kana,    null:false  
+        t.string :last_name_kana,     null:false  
         t.string :sex,                null:false
         t.string :tel,                null:false  
         t.string :email,              null: false 
