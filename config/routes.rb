168e8d7f65f3b24fcd0f6ab82d56e886921bb437
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-03-05 16:41:11 +0900

    conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-
-  get '/posts/header'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users
 end
