97279e788ae90b0799a993085b93574656f37bde
minorutaso <minorutaso@gmail.com>
2020-03-05 20:20:15 +0900

    ネーム修正前

--- db/migrate/20200305095900_change_datatypepassword_ofusers.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypepasswordOfusers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :password, :integer ,
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,33 +1,15 @@
-ActiveRecord::Schema.define(version: 2020_03_05_075535) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "zip_code", null: false
-    t.string "prefecture", null: false
-    t.string "city", null: false
-    t.string "street_num", null: false
-    t.string "building"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
-    t.string "name", default: "", null: false
-    t.string "name_kana", default: "", null: false
-    t.string "sex", null: false
-    t.string "tel", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["tel"], name: "index_users_on_tel", unique: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
