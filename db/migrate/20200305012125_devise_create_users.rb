4da463816a62ea148c0fd024beb13f019f5d83c5
otsukayu-ta <abc.abbreviation.xyz@gmail.com>
2020-03-06 18:06:06 +0900

    新規登録ページhaml

--- db/migrate/20200305012125_devise_create_users.rb
+++ db/migrate/20200305012125_devise_create_users.rb
@@ -2,8 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     def change
       create_table :users do |t|
         t.string :nickname,           null:false  
-        t.string :first_name,         null:false
-        t.string :last_name,          null:false
+        t.string :name,               null:false
+        t.string :name_kana,          null:false
         t.string :first_name_kana,    null:false  
         t.string :last_name_kana,     null:false  
         t.string :sex,                null:false
