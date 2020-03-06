11aab88135fe1cff49cd3c7bb4bff21d805fea35
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-06 10:44:38 +0900

    top-page-cimbine
    footerを作成
    posts/index.html.hamlにてrenderを使って呼び出して結合

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   
   
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  # protected
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 
   private
   def production?
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  root "posts#index"
   resources :users
+  resources :posts
 end
