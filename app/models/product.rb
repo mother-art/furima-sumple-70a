b285ecc2ef7d48cffaf567cee2237ed886192d0d
minorutaso <minorutaso@gmail.com>
2020-03-06 10:32:55 +0900

    コメントアウト

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ApplicationRecord
     belongs_to :user
     has_many :comments
     # has_many :images
+    #画像実装の時に使用
     # has_many :main_tags
+    #タグ実装の時に使用
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
     has_many :comments
     has_one :address
     # has_one :card
+    #カード機能実装の時に使用
     # has_one :sns_credential
+    #SNS機能実装の時に使用
 end
