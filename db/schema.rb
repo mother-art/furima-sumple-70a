357746d3d5e66732bd09aa9b8a3ed8be851edb8f
minorutaso <minorutaso@gmail.com>
2020-03-06 10:22:59 +0900

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_05_122602) do
-
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "zip_code", null: false
