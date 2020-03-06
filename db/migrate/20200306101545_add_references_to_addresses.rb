ff31b73963d0847dcda51b09f0a3a83cd9154002
minorutaso <minorutaso@gmail.com>
2020-03-06 21:12:41 +0900

    外部制約実装

---
+++ db/migrate/20200306101545_add_references_to_addresses.rb
@@ -0,0 +1,5 @@
+class AddReferencesToAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :addresses, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_081016) do
+ActiveRecord::Schema.define(version: 2020_03_06_120023) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.string "zip_code", null: false
     t.string "prefecture", null: false
     t.string "city", null: false
@@ -21,18 +20,20 @@ ActiveRecord::Schema.define(version: 2020_03_06_081016) do
     t.string "building"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "user_id", null: false
     t.integer "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.string "item_name", null: false
     t.string "detail", null: false
     t.string "category"
@@ -44,6 +45,8 @@ ActiveRecord::Schema.define(version: 2020_03_06_081016) do
     t.string "ship_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -60,10 +63,12 @@ ActiveRecord::Schema.define(version: 2020_03_06_081016) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
     t.string "encrypted_password"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
+  add_foreign_key "comments", "users"
+  add_foreign_key "products", "users"
 end
