45c4e51987db65a71f0cc471aa098912c16a4ef7
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-09 18:47:01 +0900

    model/address.rb修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ApplicationRecord
     validates :zip_code, presence: true, format: {with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/ , message: "全角文字は入力できません。"}
     #郵便番号ハイフン込みで登録される
     #半角数字のみ
-    belongs_to: user, optional: true
+    belongs_to :user, optional: true
     #optionalは外部キーがnullでも許可するオプション
     #wizで使用する、使用しないなら削除
 end
