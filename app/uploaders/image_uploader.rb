aecb372906ff1a0022ed6705421eef2630430d2e
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-09 18:51:58 +0900

    image_uploader.rb修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  rocess resize_to_fit: [100, 100]
+  process resize_to_fit: [100, 100]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
