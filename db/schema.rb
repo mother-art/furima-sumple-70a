99bbf2d6061afdd936487809e780d0d820d38280
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-03-11 18:24:16 +0900

    とりあえずgem
    ancestry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_105859) do
+ActiveRecord::Schema.define(version: 2020_03_10_032931) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_105859) do
     t.string "category"
     t.integer "price", null: false
     t.string "item_status", null: false
-    t.integer "postage_cost", null: false
+    t.string "postage_cost", null: false
     t.string "ship_area", null: false
     t.string "ship_method", null: false
     t.string "ship_date", null: false
