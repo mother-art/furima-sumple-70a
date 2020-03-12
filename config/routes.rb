39892e703ad7d61cfea61765cb500aedfd38b6a2
otsukayu-ta <abc.abbreviation.xyz@gmail.com>
2020-03-12 11:19:34 +0900

    カード登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -12,4 +14,11 @@ Rails.application.routes.draw do
   resources :products 
   resources :ad
   resources :comments
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
