88d6960c0e666a93d64734c7592ea67c78158ca8
minorutaso <minorutaso@gmail.com>
2020-03-12 11:41:53 +0900

    タグのアソシエーションをコメントアウト

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,9 @@
 class Product < ApplicationRecord
     validates :price, presence: true, format: {with: /\A[0-9]+\z/, message: "全角数字は入力できません。"}
-    # belongs_to :user
     has_many :comments
-    # has_many :images
-    #画像機能
     belongs_to :user
-    belongs_to :main_tag
+    # belongs_to :main_tag
+    #タグ機能実装時
     has_many :item_images
     accepts_nested_attributes_for :item_images, allow_destroy: true
-    # has_many :main_tags
-    #タグ機能
 end
