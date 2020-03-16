4cc767bf030ac93a5950a8598b6e1a5712678f7c
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-16 17:14:04 +0900

    add destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  
+  before_action :set_product, only: [:destroy]
+
   def index
     @products = Product.includes(:user).page(params[:page]).per(20).order("created_at DESC")
   end
