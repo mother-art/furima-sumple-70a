095217d5320dbccb587736ffde5cdd247b4a182a
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-19 18:49:51 +0900

    conflict fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:destroy]
 
   def index
     @products = Product.includes(:user).page(params[:page]).per(20).order("created_at DESC")
@@ -7,10 +6,9 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-  end
-
-  def search
-    @products = Product.search(params[:search]).page(params[:page]).per(20).order("created_at DESC")
+    @product_name = MainTag.find(@product.main_tag_id).name
+    @product_name2 = MainTag.find(@product.main_tag_id).parent.name
+    @product_name3 = MainTag.find(@product.main_tag_id).parent.parent.name
   end
 
   def new
@@ -35,44 +33,54 @@ class ProductsController < ApplicationController
     if @product.save
       redirect_to root_path
     else
+      @category_parent_array = ["---"]
+      MainTag.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+      end
       render :new
     end
   end
 
   def edit
     @product = Product.find(params[:id])
+    @category_parent_array = ["---"]
+    MainTag.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
   end
 
   def update
-    product = Product.find(params[:id])
-    if product.update(product_params)
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+
     else
       render :edit
     end
   end
 
-  def destroy
-    if @product.destroy
-    redirect_to products_path, notice: '出品した商品を削除しました'
-    else
-    flash.now[:alert] = '商品を削除できませんでした'
-    render :index
-    end
+    def destroy
+      product = Product.find(params[:id])
+      if product.destroy
+      redirect_to products_path, notice: '出品した商品を削除しました'
+      else
+      flash.now[:alert] = '商品を削除できませんでした'
+      render :index
+      end
   end
 
   def buyer
   end
 
+  def search
+    @products = Product.search(params[:search]).page(params[:page]).per(20).order("created_at DESC")
+  end
+
   private
   def product_params
-    params.require(:product).permit(:item_name, :detail, :category, :price, :item_status, :postage_cost, :ship_area, :ship_method, :ship_date, item_images_attributes: [:src, :_destroy, :id]).merge(seller_id: current_user.id, user_id: current_user.id)
+    params.require(:product).permit(:item_name, :detail, :main_tag_id, :price, :item_status, :postage_cost, :ship_area, :ship_method, :ship_date, item_images_attributes: [:src]).merge(seller_id: current_user.id, user_id: current_user.id)
   end
-
+  
   def set_product
     @product = Product.find(params[:id])
-  end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
   end
 end
