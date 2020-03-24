ef1e3e639060146ae93dcc26b5a394fb4c60cf3b
otsukayu-ta <abc.abbreviation.xyz@gmail.com>
2020-03-24 13:01:43 +0900

    コントローラー　修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @products = User.find(params[:id]).products
+    @products = @user.products
   end
 
   def likes
     @user = User.find(params[:id])
     @likes = Like.where(user_id: @user.id)
-    @products = User.find(params[:id]).products
+    @products = @user.products
   end
 end
