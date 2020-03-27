6db786cc55dc5e2dee9edec0cdf17e171cc29138
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-03-27 16:28:16 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@
 lock '3.12.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima-sample-70a'
+set :application, 'furima-sumple-70a'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:mother-art/furima-sample-70a.git'
+set :repo_url,  'git@github.com:mother-art/furima-sumple-70a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "furima-sample-70a_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "furima-sumple-70a_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
