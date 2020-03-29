fcc4941ab407a7609ba46848aa85030a5ca4083f
YukiSuzuki67 <j7.h.snw.ggg@gmail.com>
2020-03-29 14:59:58 +0900

    git

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_035443) do
+ActiveRecord::Schema.define(version: 2020_03_19_034504) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -85,15 +85,6 @@ ActiveRecord::Schema.define(version: 2020_03_19_035443) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "first_name", null: false
@@ -119,5 +110,4 @@ ActiveRecord::Schema.define(version: 2020_03_19_035443) do
   add_foreign_key "comments", "users"
   add_foreign_key "item_images", "products"
   add_foreign_key "products", "users"
-  add_foreign_key "sns_credentials", "users"
 end
